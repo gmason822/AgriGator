ec3506fc4bca57d254a0f4446ad5047eb0cca841
Andrew Boyd <Androyddd@gmail.com>
2016-03-13 11:56:57 -0700

    Final homepage

--- meteor/client/templates/register.js
+++ meteor/client/templates/register.js
@@ -3,5 +3,13 @@ Template.register.events({
         event.preventDefault();
         var emailVar = event.target.registerEmail.value;
         var passwordVar = event.target.registerPassword.value;
+	var businessVar = event.target.registerBusiness.value;
+	var phoneVar = event.target.phoneNumber.value;
+	Accout.createUser({
+		email: emailVar,
+		password: passwordVar,
+		business: businessVar,
+		phone: phoneVar
+	});
     }
 });
