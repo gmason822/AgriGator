6081d459d908bbe7c497c7e1f4ef673d6eb56d9b
Gmason <gmason822@gmail.com>
2016-03-13 14:43:30 -0700

    routes update and homepage work

--- meteor/routes.js
+++ meteor/routes.js
@@ -1,4 +1,26 @@
 Router.route('/', function () {
   this.render('Home');
 });
+/*
+Router.route('/profile/:user_id', function () {
+    console.log(Meteor.users.findOne(this.params.user_id))
+    this.render('Profile', {
+	data: function () 
+	{
+	    return {user: Meteor.users.findOne(this.params.user_id)}
+	}
+    });
+});
+*/
+
+Router.route('/profile', function () {
+    this.render('Profile');
+});
         
+Router.route('/field_data', function () {
+    this.render('Field Data');
+});
+
+Router.route('/field_search', function () {
+    this.render('Field Search');
+});
